382cdfabe9ceb5646d530417f6eafb97795ca492
alonshini <alonshini@gmail.com>
2017-11-27 07:50:45 +0200

    first commit

---
+++ Assets/Scripts/XMLLetters.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+using System;
+
+[XmlRoot("XMLLetters")]
+public class XMLLetters {
+
+	[XmlArray("letters")]
+	[XmlArrayItem("letter")]
+	public string[] letters;
+
+
+
+
+	public IEnumerator LoadFromResources(string path){
+		TextAsset xmlDataFile = new TextAsset ();
+		xmlDataFile = (TextAsset)Resources.Load (path, typeof(TextAsset));
+		MemoryStream ms = new MemoryStream (xmlDataFile.bytes);
+
+		XmlTextReader reader;
+		yield return reader = new XmlTextReader  (ms);
+
+		SerializeLevelFromXML (path ,reader);
+		yield return true;
+	}
+
+	private void SerializeLevelFromXML(string path, XmlReader reader)
+	{
+		Dictionary<char, string> ltrs = new Dictionary<char, string>();
+
+		var serializer = new XmlSerializer(typeof(XMLLetters));
+		XMLLetters xmlLevel = serializer.Deserialize(reader) as XMLLetters;
+
+		foreach (string ltr in xmlLevel.letters) {
+			ltrs.Add (ltr[0], ltr);
+		}
+
+
+
+	}
+}
